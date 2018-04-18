e4564fb211a9de22497f151f5916d3d736bfec2f
tzyiii <zhengyt@uci.edu>
2018-04-17 20:10:06 -0400

    add picture gallery

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,55 +1,58 @@
 import React from 'react';
-import { Tabs, Button, Spin } from 'antd';
-import {GEO_OPTIONS, POS_KEY, AUTH_PREFIX, API_ROOT, TOKEN_KEY} from '../constants.js';
 import $ from 'jquery';
-import {Gallery} from "./Gallery";
-
+import { Tabs, Spin } from 'antd';
+import { GEO_OPTIONS, POS_KEY, AUTH_PREFIX, TOKEN_KEY, API_ROOT } from '../constants';
+import { Gallery } from './Gallery';
+import { CreatePostButton } from './CreatePostButton';
 
 const TabPane = Tabs.TabPane;
-const operations = <Button>Extra Action</Button>;
 
 export class Home extends React.Component {
     state = {
-        post:[],
-        error: '',
-        loadingPost: false,
         loadingGeoLocation: false,
+        loadingPosts: false,
+        error: '',
+        posts: [],
     }
 
     componentDidMount() {
+        this.setState({ loadingGeoLocation: true, error: '' });
+        this.getGeoLocation();
+    }
+
+    getGeoLocation = () => {
         if ("geolocation" in navigator) {
-            this.setState({loadingGeoLocation:true, error : ''});
             navigator.geolocation.getCurrentPosition(
                 this.onSuccessLoadGeoLocation,
-                this.onFailedLoadGeoLocation,
+                this.onFailedLoadGeolocation,
                 GEO_OPTIONS,
             );
         } else {
-            this.setState({error : 'Can not get your location'});
+            this.setState({ error: 'Your browser does not support geolocation!' });
         }
     }
 
     onSuccessLoadGeoLocation = (position) => {
         console.log(position);
-        this.setState({loadingGeoLocation: false, error : ''});
-        const{ latitude : lat, longitude : lon } = position.coords;
-        localStorage.setItem(POS_KEY, JSON.stringify({ lat:lat, lon:lon }));
-        this.loadNearByPost();
+        this.setState({ loadingGeoLocation: false, error: '' });
+        const { latitude, longitude } = position.coords;
+        localStorage.setItem(POS_KEY, JSON.stringify({lat: latitude, lon: longitude}));
+        this.loadNearbyPosts();
     }
 
-    onFailedLoadGeoLocation = (error) => {
-        this.setState({loadingGeoLocation: false, error : 'Failed to get geolocation'});
+    onFailedLoadGeolocation = () => {
+        this.setState({ loadingGeoLocation: false, error: 'Failed to load geo location!' });
     }
 
-    getGalleryContent = () => {
+    getGalleryPanelContent = () => {
         if (this.state.error) {
             return <div>{this.state.error}</div>;
         } else if (this.state.loadingGeoLocation) {
-            return <Spin tip = 'Loading your location...'/>
-        } else if (this.state.loadingPost) {
-            return <Spin tip='Loading post...'/>
-        } else if (this.state.post && this.state.post.length > 0) {
-            const  images = this.state.post.map((post) => {
+            return <Spin tip="Loading geo location..."/>;
+        } else if (this.state.loadingPosts) {
+            return <Spin tip="Loading posts..."/>;
+        } else if (this.state.posts && this.state.posts.length > 0) {
+            const images = this.state.posts.map((post) => {
                 return {
                     user: post.user,
                     src: post.url,
@@ -59,42 +62,44 @@ export class Home extends React.Component {
                     caption: post.message,
                 }
             });
-            console.log(images);
             return <Gallery images={images}/>;
         }
-        return null;
+        else {
+            return null;
+        }
     }
 
-    loadNearByPost = () => {
-        //const{lat, lon}  = JSON.parse(localStorage.getItem(POS_KEY));
-        const lat = 37.7915953;
-        const lon = -122.3937977;
-        this.setState({loadingPost: true});
-        $.ajax({
+    loadNearbyPosts = () => {
+        const { lat, lon } = JSON.parse(localStorage.getItem(POS_KEY));
+        this.setState({ loadingPosts: true, error: ''});
+        return $.ajax({
             url: `${API_ROOT}/search?lat=${lat}&lon=${lon}&range=20`,
             method: 'GET',
-            headers :{
-                Authorization: `${AUTH_PREFIX} ${localStorage.getItem(TOKEN_KEY)}`,
-            }
-
+            headers: {
+                Authorization: `${AUTH_PREFIX} ${localStorage.getItem(TOKEN_KEY)}`
+            },
         }).then((response) => {
-            this.setState({loadingPost: false, error:''});
+            this.setState({ posts: response, loadingPosts: false, error: '' });
             console.log(response);
-            this.setState({post : response});
         }, (error) => {
-            this.setState({loadingPost: false, error : error.responseText});
+            this.setState({ loadingPosts: false, error: error.responseText });
+            console.log(error);
         }).catch((error) => {
-            this.setState({error : error});
-        })
+            console.log(error);
+        });
     }
 
-
     render() {
+        const createPostButton = <CreatePostButton loadNearbyPosts={this.loadNearbyPosts}/>;
+
         return (
-            <Tabs tabBarExtraContent={operations} className = "main-tabs">
-                <TabPane tab="Posts" key="1">{this.getGalleryContent()}</TabPane>
+            <Tabs tabBarExtraContent={createPostButton} className="main-tabs">
+                <TabPane tab="Posts" key="1">
+                    {this.getGalleryPanelContent()}
+                </TabPane>
                 <TabPane tab="Map" key="2">Content of tab 2</TabPane>
             </Tabs>
         );
     }
 }
+
--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,9 @@
 export const API_ROOT = 'https://around-75015.appspot.com';
-export const TOKEN_KEY = 'TOKEN_KEY';
-export const POS_KEY = 'POS_KEY';
+export const TOKEN_KEY = 'TOKEN';
+export const POS_KEY = 'POS';
 export const AUTH_PREFIX = 'Bearer';
 export const GEO_OPTIONS = {
-    enableHighAccuracy: true,
-    maximumAge        : 30000,
-    timeout           : 27000,
+    enableHighAccuracy: true, // remove if it takes too long to load
+    maximumAge: 3600000,
+    timeout: 30000,
 };
